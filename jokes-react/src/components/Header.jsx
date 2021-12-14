1f4be02dc3be8d0090d2f1d60329b1c77e57f950
Garfi88 <d.madjarev@gmail.com>
2021-12-14 14:33:22 +0100

    Mui AppBar instead of Bootstrap navigation

--- jokes-react/src/components/Header.jsx
+++ jokes-react/src/components/Header.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { Link } from "react-router-dom";
+import ResponsiveAppBar from "./AppBar/AppBar";
 
 class Header extends Component {
   constructor(props) {
@@ -11,18 +9,9 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar bg="dark" variant="dark">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/facts">Facts</Nav.Link>
-            <Nav.Link href="/jokes">Jokes</Nav.Link>
-
-            {/* <Link to="/">Home</Link>
-            <Link to="/facts">Facts</Link> */}
-          </Nav>
-        </Navbar>
-      </div>
+      <>
+      <ResponsiveAppBar />
+      </>
     );
   }
 }
